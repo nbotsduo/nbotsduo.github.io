a5d701b805868df93951098c43ccd89db0a58b8f
nbotsduo <mnaimhatta@yahoo.com>
2026-07-24 16:48:58 +0800

    update ver 2.2

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  darkMode: "class", // Enables class-based dark mode
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
