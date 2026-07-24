d7cfeb75cfe6de9db5f85a4ec96647c5b98e4ee4
nbotsduo <mnaimhatta@yahoo.com>
2026-07-24 16:57:43 +0800

    update ver 2.21

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Experience from "./components/Experience";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import About from "./Components/About";
+import Skills from "./Components/Skills";
+import Experience from "./Components/Experience";
+import Projects from "./Components/Projects";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 export default function Home() {
   return (
